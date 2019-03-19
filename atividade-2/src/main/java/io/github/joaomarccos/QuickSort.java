cda4e0959e97591801fe420066b46493ed0b0424
João Marcos F <joaomarccos.ads@gmail.com>
2019-03-18 21:37:53 -0300

    Giving meaning to var names

--- atividade-2/src/main/java/io/github/joaomarccos/QuickSort.java
+++ atividade-2/src/main/java/io/github/joaomarccos/QuickSort.java
@@ -4,26 +4,26 @@ public class QuickSort<T extends Comparable<T>> extends Sorter<T> {
 
     @Override
     public void sort(T[] array) {
-        quickSort(array, 0, array.length -1);
+        quickSort(array, 0, array.length - 1);
     }
 
-    private void quickSort(T[] array, int start, int end){
-        if(start < end){
+    private void quickSort(T[] array, int start, int end) {
+        if (start < end) {
             var pivotPosition = partition(array, start, end);
-            quickSort(array, start, pivotPosition -1);
+            quickSort(array, start, pivotPosition - 1);
             quickSort(array, pivotPosition + 1, end);
         }
     }
 
     private int partition(T[] array, int start, int end) {
-        int pivot = start;
+        int pivotIndex = start;
+        T pivotValue = array[end];
         for (int left = start; left < end; left++) {
-            /* Elemento atual menor ou igual ao pivô? */
-            if (array[left].equals(array[end]) || isSmaller(array[left], array[end])) {
-                swap(array, pivot++, left);
+            if (array[left].equals(pivotValue) || isSmaller(array[left], pivotValue)) {
+                swap(array, pivotIndex++, left);
             }
         }
-        swap(array, pivot, end);
-        return pivot;
+        swap(array, pivotIndex, end);
+        return pivotIndex;
     }
 }
